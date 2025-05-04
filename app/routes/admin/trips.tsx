28c3f76b02d3b332eb9413b77f156d5c528f2503
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-05-04 17:42:15 +0500

    implement trip-details

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,7 +1,33 @@
 import { Header } from "components";
-import React from "react";
+import { getAllTrips, getTripById } from "~/appwrite/trips";
+import { parseTripData } from "~/lib/utils";
+import type { Route } from "./+types/trips";
+import type { LoaderFunctionArgs } from "react-router";
+import { load } from "@syncfusion/ej2-react-charts";
 
-const Trips = () => {
+
+
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const limit = 8;
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get('page') || '1', 10);
+  const offset = (page - 1) * limit;
+
+  const {allTrips, total} = await getAllTrips(limit, offset);
+
+  return {
+    allTrips: allTrips.map(({ $id, tripDetails, imageUrls }) => ({
+      id: $id,
+      ...parseTripData(tripDetails),
+      imageUrls: imageUrls ?? [],
+    })),
+    total
+  };
+};
+
+const Trips = ({ loaderData }: Route.ComponentProps) => {
+  const trips = loaderData?.trips as Trip[] || [];
   return (
     <main className="all-users wrapper">
       <Header
